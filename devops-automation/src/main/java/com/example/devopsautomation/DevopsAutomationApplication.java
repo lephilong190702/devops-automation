49a38946a88a8f074ceb2141372a731f09137b83
Phi Long <lephilong1907@gmail.com>
2024-04-15 16:13:04 +0700

    Long-first commit

---
+++ devops-automation/src/main/java/com/example/devopsautomation/DevopsAutomationApplication.java
@@ -0,0 +1,18 @@
+package com.example.devopsautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsAutomationApplication {
+
+	public String message() {
+		return "Welcome to CI/CD !!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAutomationApplication.class, args);
+	}
+
+}
