ab4e2e5180a4978ab09cbb652c35ac8a7d83560d
Phi Long <lephilong1907@gmail.com>
2024-04-16 10:25:43 +0700

    Update

--- devops-automation/src/main/java/com/example/devopsautomation/DevopsAutomationApplication.java
+++ devops-automation/src/main/java/com/example/devopsautomation/DevopsAutomationApplication.java
@@ -2,12 +2,14 @@ package com.example.devopsautomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DevopsAutomationApplication {
 
+	@GetMapping("/message")
 	public String message() {
 		return "Welcome to CI/CD !!!";
 	}
